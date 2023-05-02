9c7ad4fa106c2781326d7e89ef3b2d632dd41800
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-02 20:24:22 +0300

    save

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { src, dest } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+
+const concat = require('gulp-concat');
+
+const uglify = require('gulp-uglify-es').default;
+function styles() {
+  return src('./app/scss/style.scss')
+	.pipe(concat('style.min.css'))
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(dest('app/css'));
+}
+
+exports.styles = styles;
